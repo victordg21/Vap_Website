ca9f4d1452263c5784b3e903a5d6d545ae136019
Joao Victor Dias <victordg050@gmail.com>
2024-06-12 13:33:11 -0300

    first commit of the site

---
+++ src/App.vue
@@ -0,0 +1,363 @@
+<script setup>
+
+</script>
+
+<template>
+  <header class="centralize">
+    <img class="header-image" src="./assets/clock.png" alt="">
+      <h1 class="text-header span-yellow">Essa oferta terminará em XX:XX:XX</h1>
+  </header>
+  <section class="Section1 padding-section centralize">
+    <div class="container">
+      <h3 class="span-yellow">GARANTIDO PARA VESTIBULANDOS:</h3>
+      <h2 class="font-bold font-size-bigger">Alcance a verdadeira Alta Perfomance e <span class="span-yellow">seja Aprovado!</span></h2>
+      <h4 class="font-size-medium">Domine os <span class="span-yellow">4 princípios da Alta Performance</span> nos estudos:</h4>
+      <div class="">
+        <div class="centralize font-size-medium">
+          <img class="images-section-1" src="./assets/brain.png" alt="">
+          <p class="text-first-section">Força Emocional</p>
+        </div>
+        <div class="centralize font-size-medium">
+          <img class="images-section-1" src="./assets/timer.png" alt="">
+          <p class="text-first-section">Produtividade e Gestão de Tempo</p>
+        </div>
+        <div class="centralize font-size-medium">
+          <img class="images-section-1" src="./assets/books.png" alt="">
+          <p class="text-first-section">Neurociência da Aprendizagem</p>
+        </div>
+        <div class="centralize font-size-medium">
+          <img class="images-section-1" src="./assets/rain.png" alt="">
+          <p class="text-first-section">Amadurecimento e Motivação</p>
+        </div>
+      </div>
+      <button>Quero garantir meu acesso!</button>
+    </div>
+  </section>
+  <section class="Section2 padding-section centralize">
+    <div class="container">
+      <h2 class="font-size-bigger font-bold">As 4 trilhas do VAP</h2>
+      <div class="grid-informations">
+        <div class="card">
+          <div class="card-blue"></div>
+          <div class="card-gray">
+            <p class="font-card-start">Lide com a ansiedade, os sentimentos ruins e as pressões internas e extemas.</p>
+            <p class="font-bold align-end font-card-end">Força Emocional</p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-blue"></div>
+          <div class="card-gray">
+            <p class="font-card-start">Choques de realidade. Doses de ânimo. Tudo o que vooe precisa para virar gente e dar o máximo nos estudos.</p>
+            <p class="font-bold align-end font-card-end">Amadurecimento e Motivação</p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-blue"></div>
+          <div class="card-gray">
+            <p class="font-card-start">Estude conforme evidências científicas. Maximize seu aprendizado e alcance a aprovação.</p>
+            <p class="font-bold align-end font-card-end">Neurociência da Aprendizagem</p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-blue"></div>
+          <div class="card-gray">
+            <p class="font-card-start">Domine seu tempo, pare de procrastinar e se torne produtivo de verdade.</p>
+            <p class="font-bold align-end font-card-end">Gestão de Tempo</p>
+          </div>
+        </div>
+      </div>
+      <button>Quero garantir meu acesso!</button>
+      <div class="items-section">
+        <div class="items-grid">
+          <img class="grid-photo" src="" alt="">
+          <div class="items-text">
+            <h3 class="font-large">O que você <span class="span-yellow">vai encontrar?</span></h3>
+            <p class="padding-bottom" ><span class="bullet">•</span> Conteúdo em texto, rápido e prático.</p>
+            <p class="padding-bottom" ><span class="bullet">•</span> Organizado no Notion.</p>
+            <p class="padding-bottom" ><span class="bullet">•</span> Técnicas de controle emocional.</p>
+            <p class="padding-bottom" ><span class="bullet">•</span> Técnicas de antiprocrastinação.</p>
+            <p class="padding-bottom" ><span class="bullet">•</span> Métodos de estudo baseados em neurociência.</p>
+            <p class="padding-bottom" ><span class="bullet">•</span> Doses de ânimo e motivação.</p>
+          </div>
+          <img class="grid-photo" src="" alt="">
+          <div class="items-text">
+            <h3 class="font-large">O que você <span class="span-red">não vai encontrar?</span></h3>
+            <p class="padding-bottom" ><span class="bullet">•</span> Aulas chatas e cheias de enrolação.</p>
+            <p class="padding-bottom" ><span class="bullet">•</span> Mais um cronograma inútil.</p>
+            <p class="padding-bottom" ><span class="bullet">•</span> Conteúdos desnecessários.</p>
+            <p class="padding-bottom" ><span class="bullet">•</span> Templates que você nunca usa.</p>
+          </div>
+          <img class="grid-photo" src="" alt="">
+          <div class="items-text">
+            <h3 class="font-large">Para quem <span class="span-yellow">é?</span></h3>
+            <p class="padding-bottom" ><span class="bullet">•</span> Quem quer ser aprovado sem perder a cabeça.</p>
+            <p class="padding-bottom" ><span class="bullet">•</span> Quem quer dominar o medo e a ansiedade.</p>
+            <p class="padding-bottom" ><span class="bullet">•</span> Quem quer ser constante.</p>
+            <p class="padding-bottom" ><span class="bullet">•</span> Quem não tem tempo para ver aulas enormes.</p>
+            <p class="padding-bottom" ><span class="bullet">•</span> Quem quer ser produtivo de verdade.</p>
+            <p class="padding-bottom" ><span class="bullet">•</span> Quem quer se tornar uma pessoa melhor.</p>
+          </div>
+          <img class="grid-photo" src="" alt="">
+          <div class="items-text">
+            <h3 class="font-large">Para quem <span class="span-red">não é?</span></h3>
+            <p class="padding-bottom" ><span class="bullet">•</span> Quem acredita que "é só estudar que passa".</p>
+            <p class="padding-bottom" ><span class="bullet">•</span> Quem quer continuar estagnado.</p>
+            <p class="padding-bottom" ><span class="bullet">•</span> Quem quer passar anos estudando sem resultado.</p>
+            <p class="padding-bottom" ><span class="bullet">•</span> Quem quer ser refém das emoções.</p>
+          </div>
+        </div>
+        <div class="centralize">
+          <button>Quero garantir meu acesso!</button>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="Section3 padding-section centralize">
+    <div class="container">
+      <h3>COMPRE AGORA COM <span class="span-red">MAIS DE 80% DE DESCONTO</span></h3>
+      <h2 class="font-bold font-size-bigger-2">Aproveite o <span class="span-yellow">preço exclusivo</span> de lançamento</h2>
+      <img class="price-image" src="./assets/Preço.png" alt="">
+      <div>
+        <button>Quero garantir meu acesso!</button>
+      </div>
+      <p>Compre na oferta de lançamento e <span class="span-yellow">garanta todas as atualizações até o Enem.</span></p>
+    </div>
+  </section>
+  <section class="Section4 padding-section centralize">
+    <div class="display-grid-photo container">
+      <img class="grid-photo" src="./assets/Foto1.png" alt="">
+      <div class="grid-text">
+        <h2 class="h2-title font-size-bigger">Quem sou eu?</h2>
+        <p>Davi Belo de Carvalho</p><br>
+        <p>Terapeuta no maior cursinho pré-vestibular do seu estado.</p><br>
+        <p>Davi Belo já atendeu mais de 200 vestibulando e gerenciou intervenções em grupo, palestras e momentos de espiritualidade com uma série de estudantes.</p><br>
+        <p>Depois de ter escutado e ajudado muitos alunos e pacientes vestibulandos, decidiu condensar as principais necessidades e problemas que percebeu e solucionou no VAP.</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+*{
+  font-family: Helvetica, sans-serif;
+  font-weight: 500;
+}
+
+.span-yellow{
+  color: #E5B30C;
+}
+
+.span-red{
+  color:#B53535;
+}
+
+.images-section-1{
+  width: 30px;
+  padding-right: 20px;
+  padding-bottom: 10px;
+}
+
+.font-large{
+  font-size: 32px;
+  margin: 0;
+  padding-bottom: 15px;
+}
+
+.font-size-bigger{
+  font-size: 44px;
+}
+
+.font-size-bigger-2{
+  font-size: 50px;
+}
+
+.font-size-medium{
+  font-size: 24px;
+}
+
+header{
+  background-color: #B53535;
+}
+
+.header-image{
+  width: 40px;
+  padding: 10px 20px 10px 0;
+}
+
+
+
+.container {
+  max-width: 800px;
+  width: 100%;
+}
+
+.centralize{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.Section1, .Section3{
+  background-color: #101010;
+  color: white;
+}
+
+.price-image{
+  width: 350px;
+}
+
+.padding-bottom{
+  padding-bottom: 5px;
+}
+
+.bullet{
+  font-size: 10px;
+}
+
+p, .align-end{
+  margin: 0;
+}
+
+.font-card-start{
+  font-weight: 400;
+  font-size: medium;
+}
+
+.font-card-end{
+  font-weight: 500;
+  font-size: 18px;
+}
+
+button{
+  margin: 2rem;
+  padding: 10px 25px;
+  border-radius: 20px;
+  border: none;
+  font-weight: 500;
+  font-size: large;
+  background-color: #E5B30C;
+}
+
+.display-grid-photo{
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  gap: 2rem;
+}
+
+.grid-photo{
+  width: 100%;
+}
+
+.grid-text{
+  text-align: left;
+}
+
+.padding-section{
+  padding: 2rem 0;
+}
+
+.h2-title{
+  margin-top: 0;
+}
+
+.items-section{
+  background-color: #eee;
+  width: 90%;
+  margin: 0 auto;
+  border-radius: 20px;
+  text-align: left;
+}
+
+.items-grid{
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 2rem;
+  padding: 2rem;
+}
+
+.items-text{
+  list-style-type: disc;
+}
+
+.grid-informations{ 
+  padding-top: 40px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 3rem;
+}
+
+.card {
+  position: relative; 
+  height: 200px; 
+}
+
+.card-blue {
+  position: absolute;
+  background-color: blue;
+  width: 33.3%;
+  height: 100%;
+  border-radius: 20px;
+  left: 0;
+  top: 0;
+}
+
+.card-gray {
+  position: absolute;
+  text-align: left;
+  background-color: #eee;
+  width: 66.6%;
+  height: 60%;
+  border-radius: 20px;
+  padding: 1rem;
+  top: 50%;
+  left: 25%; 
+  transform: translateY(-50%);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+@media (max-width: 1080px) and (min-width: 656px){
+
+  .container{
+    width: 90%;
+  }
+}
+
+@media (max-width: 655px){
+
+  .container{
+    width: 90%;
+  }
+
+  .grid-informations{
+    display: block;
+  }
+
+  .card{
+    margin-bottom: 20px;
+  }
+
+  
+.images-section-1{
+  width: 25px;
+  padding-right: 5px;
+  padding-bottom: 10px;
+}
+
+.text-first-section{
+  font-size: 15px;
+}
+
+.header-image{
+  padding: 10px 0 10px 0;
+}
+
+header{
+  padding: 0 50px;
+}
+}
+
+</style>
